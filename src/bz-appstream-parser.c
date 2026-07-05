3b410048e25d651031d3942d9cd7e45faf0bc3de
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2026-07-05 10:37:13 +0200

    Add license header to appstream-parser.c (#1704)

--- src/bz-appstream-parser.c
+++ src/bz-appstream-parser.c
@@ -1,4 +1,22 @@
-/* bz-appstream-parser.c */
+/* bz-window.c
+ *
+ * Copyright 2025 Adam Masciola, 2026 Alexander Vanhee
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 #define G_LOG_DOMAIN  "BAZAAR::APPSTREAM-PARSER"
 #define BAZAAR_MODULE "appstream-parser"
